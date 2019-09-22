7917d6a256250bbcb215b73a8d16a2a1946c475f
Manuel Tablado <manueltabladoordoqui@gmail.com>
2019-09-22 20:42:57 -0300

    Primer Comit , Creacion de componente REGISTER (Registro de Usuario Cliente)

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+  
+})
+
+export class RegisterComponent implements OnInit {
+  hide = true;
+  firstname = new FormControl('', [Validators.required]);
+  lastname = new FormControl('', [Validators.required]);
+  birthday = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required]);
+  terms = new FormControl('', [Validators.required]); 
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
