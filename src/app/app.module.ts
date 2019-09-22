359f528c96f68d8b592cc8a3e6ff595e381af47f
emartinez <eze2411@gmail.com>
2019-09-22 17:22:42 -0300

    avance home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -16,22 +18,23 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HelloWorldComponent } from './components/hello-world/hello-world.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    SidebarComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot([
-      {path: 'hello-world',  component: HelloWorldComponent},
-      {path: '',  component: LoginComponent},
-      {path: 'home',  component: HomeComponent}
-    ]),
     HttpClientModule,
     MatCardModule,
     MatInputModule,
@@ -41,6 +44,11 @@ import { HomeComponent } from './components/home/home.component';
     MatIconModule,
     FormsModule,
     ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: 'hello-world',  component: HelloWorldComponent},
+      {path: '',  component: LoginComponent},
+      {path: 'home',  component: HomeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
