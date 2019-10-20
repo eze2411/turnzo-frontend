961d097faa63df378f2e9f62fbe50a425a156c80
emartinez <eze2411@gmail.com>
2019-10-20 20:43:13 -0300

    agrega validaciones registerForm

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { MatCheckbox } from '@angular/material/checkbox';
 
 
 @Component({
@@ -11,22 +12,43 @@ import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms'
 
 export class RegisterComponent implements OnInit {
 
+	@ViewChild('terms', { static: false }) terms;
+
 	hide = true;
 	registerForm: FormGroup;
+	isInvalidTerms: Boolean;
+
+
 
 	constructor(private fb: FormBuilder) {
 		this.registerForm = this.fb.group({
 			firstname: new FormControl('', [Validators.required]),
 			lastname: new FormControl('', [Validators.required]),
 			birthdate: new FormControl('', [Validators.required]),
-			email: new FormControl('', [Validators.required]),
+			email: new FormControl('', [Validators.required, Validators.email]),
 			password: new FormControl('', [Validators.required]),
 			terms: new FormControl('', [Validators.required])
-		  })
-	 }
+		})
+	}
 
 	ngOnInit() {
+		this.isInvalidTerms = false;
+	}
 
+	validateForm() {
+		if (this.registerForm.valid) {
+			return console.log('isValid');
+		} else {
+			if (this.registerForm.get('terms').hasError) {
+				this.isInvalidTerms = true;
+			}
+			Object.keys(this.registerForm.controls).forEach(field => this.registerForm.get(field).markAsTouched({ onlySelf: true }));
+			return console.log('invalid');
+		}
+	}
+
+	changeStatus() {
+		this.isInvalidTerms = !this.isInvalidTerms;
 	}
 
 }
