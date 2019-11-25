50657ce3803d82538f360e27eb9518ecf16b7636
emartinez <eze2411@gmail.com>
2019-11-25 00:19:17 -0300

    FRONT-96 programacion SearchComponent

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {EventService} from "../../services/event.service";
+import {AppStorageService} from "../../services/app-storage.service";
 
 @Component({
   selector: 'app-search',
@@ -9,17 +10,74 @@ import {EventService} from "../../services/event.service";
 export class SearchComponent implements OnInit, OnChanges {
     message: any;
     currentValue: string;
+    userData: any;
+    eventsData: any;
     @Input() searchString: string;
+    @Input() adminCalendarShowView: string;
+    filteredEventsData: any;
 
-  constructor(private eventService: EventService) { }
+  constructor(private eventService: EventService,
+              private storage: AppStorageService) { }
 
     ngOnInit() {
+        this.userData = this.storage.getStoredUser();
     }
 
     ngOnChanges(changes: SimpleChanges): void {
+      console.log("asdasd");
         if (changes.searchString.currentValue) {
-            console.log(changes.searchString.currentValue);
+            console.log("xcxcxc");
+            this.currentValue = changes.searchString.currentValue;
+            this.userData = this.storage.getStoredUser();
+            this.getEvents();
         }
     }
 
+    getEvents() {
+        if(this.userData) {
+            if (this.userData.role == 'ADMIN') {
+                this.getAdminEvents();
+            } else {
+                this.getUserEvents();
+            }
+        }
+    }
+
+    getAdminEvents() {
+        this.eventService.getAdminEvents()
+            .subscribe(
+                data => {
+                    this.eventsData = data.events[0];
+                    this.filterEventsData();
+                },
+                error => {
+                    //console.log(JSON.parse(error).status);
+                    //console.log(JSON.parse(error).message);
+                    // toHacer abrir snackbar ocurrio un error
+                }//,
+                //() => console.log(this.calendarData.events)
+            );
+    }
+
+    getUserEvents() {
+        this.eventService.getUserEventsByAdmin(this.adminCalendarShowView, this.userData.email)
+            .subscribe(
+                data => {
+                    this.eventsData = data.events[0];
+                    this.filterEventsData();
+                },
+                error => {
+                    //console.log(JSON.parse(error).status);
+                    //console.log(JSON.parse(error).message);
+                    // toHacer abrir snackbar ocurrio un error
+                }//,
+                //() => console.log(this.calendarData.events)
+            );
+    }
+
+
+    private filterEventsData() {
+      let current = this.currentValue.toLowerCase();
+      this.filteredEventsData = this.eventsData.filter(event => (event.title.toLowerCase().indexOf(current) > -1 || event.event_description.toLowerCase().indexOf(current) > -1));
+    }
 }
